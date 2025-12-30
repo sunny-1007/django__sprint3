4124f8b1f485f97a9cd654ff483d1cb50fedabf5
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 19:54:07 +0300

    Fix docstrings in pages views (D300/Q002)

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def about(request):
-    '''Render the About page.'''
-    template = 'pages/about.html'
+    """Render the About page."""
+    template = "pages/about.html"
     return render(request, template)
 
 
 def rules(request):
-    '''Render the Rules page.'''
-    template = 'pages/rules.html'
+    """Render the Rules page."""
+    template = "pages/rules.html"
     return render(request, template)
