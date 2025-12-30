7086f4e30a3b3ee094c725460ed03de361eb8bd1
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 20:13:19 +0300

    Fix imports order in project urls (isort)

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('pages/', include('pages.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("pages/", include("pages.urls")),
 ]
+
