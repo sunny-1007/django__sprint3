9a7dfd88fd577a094e830b29b3140c4348272f2d
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 18:47:04 +0300

    Fix blog views: imports, docstrings, isort (ABS101, D103)

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,47 +1,49 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
-from .models import Post, Category
+
+from blog.models import Category, Post
 
 
 def index(request):
-    template = 'blog/index.html'
-    post_list = Post.objects.select_related(
-        'category', 'location', 'author'
-    ).filter(
-        pub_date__lte=timezone.now(),
+    '''Render the main page with a list of published posts.'''
+    post_list = Post.objects.filter(
         is_published=True,
-        category__is_published=True
-    )[:5]
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    ).order_by('-pub_date')
+    template = 'blog/index.html'
     context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    template = 'blog/detail.html'
+    '''Render the details page for a single post.'''
     post = get_object_or_404(
-        Post.objects.select_related('category', 'location', 'author'),
-        id=post_id,
-        pub_date__lte=timezone.now(),
+        Post,
+        pk=post_id,
         is_published=True,
-        category__is_published=True
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
     )
+    template = 'blog/detail.html'
     context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
+    '''Render a page with posts filtered by category.'''
     category = get_object_or_404(
         Category,
         slug=category_slug,
-        is_published=True
+        is_published=True,
     )
-    post_list = Post.objects.select_related(
-        'category', 'location', 'author'
-    ).filter(
-        category=category,
+    post_list = category.posts.filter(
+        is_published=True,
         pub_date__lte=timezone.now(),
-        is_published=True
-    )
-    context = {'category': category, 'post_list': post_list}
+    ).order_by('-pub_date')
+    template = 'blog/category.html'
+    context = {
+        'category': category,
+        'post_list': post_list,
+    }
     return render(request, template, context)
