4dfe5d7a68b09b3e9fb3c3463eee72d9f87cddc5
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 19:53:38 +0300

    Fix isort in pages urls (I004)

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from pages import views
 
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('rules/', views.rules, name='rules'),
+    path("about/", views.about, name="about"),
+    path("rules/", views.rules, name="rules"),
 ]
