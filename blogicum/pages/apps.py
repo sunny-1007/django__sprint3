851400176ac698d0f22d4c7f1d0dcddda7540161
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 18:42:43 +0300

    Add docstring to PagesConfig (D101)

--- blogicum/pages/apps.py
+++ blogicum/pages/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class PagesConfig(AppConfig):
+    '''Application configuration for the Pages app.'''
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
