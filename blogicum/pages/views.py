ded21251339362d38d5fa8ad651d558fcf6358fb
sunny-1007 <m2416511@edu.misis.ru>
2025-12-30 18:36:59 +0300

    Add docstrings to pages views (D103)

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 
 def about(request):
+    '''Render the About page.'''
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request):
+    '''Render the Rules page.'''
     template = 'pages/rules.html'
     return render(request, template)
